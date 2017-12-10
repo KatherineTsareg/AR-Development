5036b41e151df58ec2800a2d16c68736fafdeaeb
CathyTsar <tennleston@gmail.com>
2017-12-10 14:53:24 +0300

    first commit

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scripts
+{
+    public class MenuScript : MonoBehaviour
+    {
+        public void LoadByIndex(int sceneIndex)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+
+        public void LoadNextScene()
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
